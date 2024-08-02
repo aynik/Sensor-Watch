198cbbeaf23be20b3c35594b15cc13b7024499fc
Pablo V <maybe_pablo+github@icloud.com>
2024-08-03 01:35:35 +0900

    Add beeps face

--- movement/movement_faces.h
+++ movement/movement_faces.h
@@ -105,6 +105,7 @@
 #include "tuning_tones_face.h"
 #include "kitchen_conversions_face.h"
 #include "metronome_face.h"
+#include "beeps_face.h"
 // New includes go above this line.
 
 #endif // MOVEMENT_FACES_H_
